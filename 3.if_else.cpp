6f6fe2e6591408111e5482742ec0004aea2840b2
kaushlendratiwari <84301459+kaushlendratiwari@users.noreply.github.com>
2021-12-20 14:30:56 +0530

    Add files via upload

---
+++ 3.if_else.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"Enter the number :";
+cin>>n;
+if (n%2==0)
+{
+    cout<<"The given number is " <<n<<" is even";
+}
+
+else{
+    cout<<"The given number is " <<n<<" is odd";
+}
+
+return 0;
+
+
+}
